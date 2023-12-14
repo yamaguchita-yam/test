ff2a06a5f0ea2a5d65efbbc230180818c2d22726
yamaguchita-yam <153715512+yamaguchita-yam@users.noreply.github.com>
2023-12-14 11:15:54 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+// test/test.js
+
+const assert = require('assert');
+const { myFunction } = require('../index');
+
+describe('MyFunction', () => {
+  it('should return "Hello, GitHub Actions!"', () => {
+    const result = myFunction();
+    assert.strictEqual(result, 'Hello, GitHub Actions!');
+  });
+});
